4af92aefb55d969214de3bc520b5ef3d1f706647
nick.gnmc <nick.sutton@gbmc.ac.uk>
2025-08-26 13:46:00 +0100

    Added basic conversion function and valid data testing

--- assets/js/temperature-convert.js
+++ assets/js/temperature-convert.js
@@ -1,1 +1,6 @@
- 
+function fahrenheitToCelicus(f) {
+    let c = (f - 32) * 5/9;
+    return c;
+}
+
+module.exports = { fahrenheitToCelicus };
--- assets/js/tests/temperature-convert.test.js
+++ assets/js/tests/temperature-convert.test.js
@@ -1,1 +1,19 @@
- 
+const {fahrenheitToCelicus}  = require("../temperature-convert.js");
+
+describe ("Temperature Conversion", () => {
+    test("Should return a temperature of 212", () =>{
+        expect(fahrenheitToCelicus(212)).toBe(100);
+    });
+    test("Should return a temperature of 0", () =>{
+        expect(fahrenheitToCelicus(32)).toBe(0);
+    });
+    test("Should return a temperature of 0", () =>{
+        expect(fahrenheitToCelicus(32)).toBe(0);
+    });
+    test("Should return a temperature of -17.7778", () =>{
+        expect(fahrenheitToCelicus(0).toFixed(4)).toBe("-17.7778");
+    });
+    test("Should return a temperature of -20.5556", () =>{
+        expect(fahrenheitToCelicus(-5).toFixed(4)).toBe("-20.5556");
+    });
+});
